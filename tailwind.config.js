f698614a67c0f3c684a1e4d3cd95e299724e46fe
Galih Roswandi <galihroswandi25@gmail.com>
2023-01-25 05:53:49 +0700

    Video 13 Membuat website portfolio tinggal responsive about dan seterusnya

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
-  darkMode: 'class',
+  content: ['./index.html'],
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
     extend: {
-      spacing: {
-        13: '3.25rem',
-      },
-      fontFamily: {
-        Inter: ['Inter']
-      },
       colors: {
-        primary: "#bada55",
-        kopi: "C0FFE"
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#0f172a'
       },
-      animation: {
-        'spin-slow': 'spin 3s linear infinite',
-        'waving-hand': 'wave 2s linear infinite',
-      },
-      keyframes: {
-        wave: {
-          '0%': { transform: 'rotate(0.0deg)' },
-          '10%': { transform: 'rotate(14deg)' },
-          '20%': { transform: 'rotate(-8deg)' },
-          '30%': { transform: 'rotate(14deg)' },
-          '40%': { transform: 'rotate(-4deg)' },
-          '50%': { transform: 'rotate(10.0deg)' },
-          '60%': { transform: 'rotate(0.0deg)' },
-          '100%': { transform: 'rotate(0.0deg)' },
-        },
-      },
-    }
+      screens: {
+        '2xl': '1320px'
+      }
+    },
   },
   plugins: [],
 }
